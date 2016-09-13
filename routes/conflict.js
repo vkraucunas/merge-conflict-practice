aa878990ab9c61fb0d45a9026f0e7e3193c3299f
Teagan Glenn <that@teagantotally.rocks>
2016-09-13 10:21:07 -0600

    Conflicts

---
+++ routes/conflict.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET listing. */
+router.get('/', function(req, res, next) {
+    res.send('respond with a resource');
+});
+
+router.put('/', function(req, res, next) {
+    res.send('We don\'t have this implemented');
+    next();
+});
+
+router.post('/', function(req, res, next) {
+    res.send('Put complete.');
+});
+
+router.delete('/', function(req, res, next) {
+    res.send('Delete complete.');
+});
+
+module.exports = router;
