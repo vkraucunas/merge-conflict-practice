e2865a38dc3e9f9d44efb2488778724d2badf64e
Teagan Glenn <that@teagantotally.rocks>
2016-09-13 10:15:39 -0600

    Accounts file

---
+++ routes/accounts.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET accounts listing. */
+router.get('/', function(req, res, next) {
+    res.send('respond with a resource');
+});
+
+router.put('/', function(req, res, next) {
+    throw Error("Not Implemented.")
+});
+
+module.exports = router;
